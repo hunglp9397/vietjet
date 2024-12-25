0c2da7a99504e3148016c0b4202a4f88bc81028b
hunglp9397 <lephihung0997@gmail.com>
2024-12-25 20:48:50 +0700

    Refactor

--- src/main/java/com/booking/vietjet/config/SecurityConfig.java
+++ src/main/java/com/booking/vietjet/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
@@ -22,7 +23,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(authz -> authz
                 .anyRequest().permitAll())
                 .addFilterBefore(idempotenceFilter, ChannelProcessingFilter.class)
-                .csrf(csrf -> csrf.disable());
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
